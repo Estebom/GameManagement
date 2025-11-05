9a027709d6b71f831ff6692ef9fdfcffd2bc7bbd
Estebom <estebomofficial@gmail.com>
2025-11-04 23:29:12 -0500

    commit

---
+++ GameManagement/Data/GameServiceConnector.cs
@@ -0,0 +1,61 @@
+﻿using GameManagement.Data.Models;
+using System.Collections.Generic;
+
+namespace GameManagement.Data
+{
+    public class GameServiceConnector
+    {
+        HttpClient httpClient;
+        private readonly string endpoint = "http://localhost:5181/";
+        public GameServiceConnector()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(endpoint);
+        }
+
+        public async Task<List<Game>> GetGameAsync() 
+        {
+            HttpResponseMessage response = await httpClient.GetAsync("api/game");
+            List<Game> games = new List<Game>();
+            if (response.IsSuccessStatusCode) 
+            {
+               games = await response.Content.ReadFromJsonAsync<List<Game>>();
+            }
+
+            return games;
+        }
+
+        public async Task<Game> GetGameAsync(Guid id) 
+        {
+            HttpResponseMessage response = await httpClient.GetAsync($"api/game/{id}");
+            
+            if (response.IsSuccessStatusCode) 
+            {
+                Game game = await response.Content.ReadFromJsonAsync<Game>();
+                return game;
+            }
+            return new Game() {Name = null };
+        }
+
+        public async Task<bool> UpdateGameAsync(Guid id, GameDTO gameDTO) 
+        {
+            HttpResponseMessage response = await httpClient.PutAsJsonAsync($"api/game/{id}", gameDTO);
+
+            if (response.IsSuccessStatusCode) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> DeleteGameAsync(Guid id) 
+        {
+            HttpResponseMessage response = await httpClient.DeleteAsync($"api/game/{id}");
+            if (response.IsSuccessStatusCode) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ GameManagement/Data/Models/GameDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GameManagement.Data.Models
+{
+    public class GameDTO
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
